65bfbfc43634f85700e3b017cfc6aaba0ddeb3e9
Henil Malaviya <henilm.dev@gmail.com>
2024-09-29 13:49:31 +0530

    feat: setup & integrate husky

---
+++ commitlint.config.cjs
@@ -0,0 +1,25 @@
+/* eslint-disable no-undef */
+module.exports = {
+	extends: ["@commitlint/config-conventional"],
+	rules: {
+	  "type-enum": [
+		2,
+		"always",
+		[
+		  "feat",
+		  "fix",
+		  "wip",
+		  "docs",
+		  "chore",
+		  "style",
+		  "refactor",
+		  "ci",
+		  "test",
+		  "revert",
+		  "perf",
+		  "vercel",
+		],
+	  ],
+	},
+  };
+  
