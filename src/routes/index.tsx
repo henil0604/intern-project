9ec40bb2b09722b51f9de86a3c94d1da45c6db71
Henil Malaviya <henilm.dev@gmail.com>
2024-10-02 22:49:27 +0530

    feat: basic setup for react router

---
+++ src/routes/index.tsx
@@ -0,0 +1,30 @@
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+
+import { AuthLoader, RootLayout } from "@/components/layouts/root";
+
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<RootLayout />} loader={AuthLoader}>
+        <Route
+          path="*"
+          lazy={async () => ({
+            Component: () => <div>Not Found</div>,
+          })}
+        />
+        <Route
+          index
+          lazy={async () => {
+            return {
+              Component: (await import("@/routes/Home")).HomePage,
+            };
+          }}
+        />
+      </Route>
+    </>,
+  ),
+);
